9212dc3a6064082a3bfdf253353650c0739852ba
Comebackism <mzamzababatv@gmail.com>
2026-08-12 11:01:16 +0700

    revert: remove ssl config from db

--- backend/config/db.js
+++ backend/config/db.js
@@ -7,9 +7,6 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  ssl: process.env.DB_HOST && process.env.DB_HOST.includes('neon.tech') 
-        ? { rejectUnauthorized: false } 
-        : false,
 });
 
 pool.query('SELECT NOW()', (err, res) => {
